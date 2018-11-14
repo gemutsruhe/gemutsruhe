7fef7fd64613f4872a9164aad630075c9222d6fc
gemutsruhe <44457864+gemutsruhe@users.noreply.github.com>
2018-11-14 16:22:00 +0900

    Add files via upload

--- 20183662.c
+++ 20183662.c
@@ -2,24 +2,24 @@
 
 void indexAddMat(int Mat[])
 {
-	for(int i = 0; i < 9; i++) *(Mat + i + 18) = (*(Mat + i)) + (*(Mat + 9 + i));
+	int i;
+	for(i = 0; i < 9; i++) Mat[18 + i] = Mat[i] + Mat[9 + i];
 }
 
 
 void ptrProdMat(int *Mat)
 {
-	for(int i = 0; i < 27; i++) *(Mat + i / 3 + 18) += (*(Mat + 3 * (i / 9) + i % 3)) * (*(Mat + 9 + (i / 3) % 3 + 3 * (i % 3)));
+	int i;
+	for(i = 0; i < 27; i++) *(Mat + i / 3 + 18) += (*(Mat + 3 * (i / 9) + i % 3)) * (*(Mat + 9 + (i / 3) % 3 + 3 * (i % 3)));
 }
 
 int main()
 {
 	int mat[27] = {0,};
-	int *p_mat = mat;
-	int *p_mat1 = mat;
-	int *p_mat2 = mat+9;
+	int i;
 	
 	printf("enter 3 X 3 matrix element : ");
-	for(int i = 0; i < 9; i++)
+	for(i = 0; i < 9; i++)
 	{
 		scanf("%d",mat+i);
 		*(mat + 9 + 3 * (i % 3) + i / 3) = *(mat + i);
@@ -27,18 +27,18 @@ int main()
 	
 	printf("\nsum of matrix\n");
 	indexAddMat(mat);
-	for(int i = 0; i < 9; i++)
+	for(i = 0; i < 9; i++)
 	{
 		printf("%4d",*(mat+18+i));
 		if(i % 3 == 2) printf("\n");
 	}
 	printf("\n");
 	
-	for(int i = 18; i < 27; i++) *(mat+i) = 0;
+	for(i = 18; i < 27; i++) *(mat+i) = 0;
 	
 	printf("product of matrix\n");
 	ptrProdMat(mat);
-	for(int i = 0; i < 9; i++)
+	for(i = 0; i < 9; i++)
 	{
 		printf("%4d",*(mat+18+i));
 		if(i % 3 == 2) printf("\n");
@@ -46,4 +46,3 @@ int main()
 	
 	return 0;
 }
-
