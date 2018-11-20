5b2b9625bec78df2f0885c3de73111740993ad75
gemutsruhe <44457864+gemutsruhe@users.noreply.github.com>
2018-11-20 21:07:24 +0900

    Add files via upload
    8주차

--- 20183662.c
+++ 20183662.c
@@ -1,48 +1,114 @@
 #include <stdio.h>
+#include <malloc.h>
 
-void indexAddMat(int Mat[])
+typedef struct birth
 {
-	int i;
-	for(i = 0; i < 9; i++) Mat[18 + i] = Mat[i] + Mat[9 + i];
+	int year;
+	int month;
+	int date;
+}birth;
+
+typedef struct private_statement
+{
+	char name[8];
+	birth birth_data;
+	char gender[8];
+	int phone_number;
+}p_s;
+
+void swap_int(int *data1, int *data2)
+{
+	int temp = *data1;
+	*data1 = *data2;
+	*data2 = temp;
 }
 
-
-void ptrProdMat(int *Mat)
+void swap_p_p_s(p_s **data1, p_s **data2)
 {
-	int i;
-	for(i = 0; i < 27; i++) *(Mat + i / 3 + 18) += (*(Mat + 3 * (i / 9) + i % 3)) * (*(Mat + 9 + (i / 3) % 3 + 3 * (i % 3)));
+	p_s *temp = *data1;
+	*data1 = *data2;
+	*data2 = temp; 
+}
+
+void name_priority_sort(p_s **p_p_s, int *birth, int num)
+{
+	int i, j, k;
+	for(i = 0; i < num - 1; i++)
+	{
+		for(j = i + 1; j < num; j++)
+		{
+			k = 0;
+			while(*((char *)*(p_p_s + i) + k) == *((char *)*(p_p_s + j) + k)) k++;
+			if(*((char *)*(p_p_s + i) + k) < *((char *)*(p_p_s + j) + k))
+			{
+				swap_p_p_s(p_p_s + i, p_p_s + j);
+				swap_int(birth + i, birth + j);
+			}
+		}
+	}
+}
+
+void birth_priority_sort(p_s **p_p_s, int *birth, int num)
+{
+	int i, j;
+	for(i = 0; i < num - 1; i++)
+	{
+		for(j = i + 1; j < num; j++)
+		{
+			if(*(birth + i) > *(birth + j))
+			{
+				swap_p_p_s(p_p_s + i, p_p_s + j);
+				swap_int(birth + i, birth + j);
+			}
+			
+		}
+	}
+}
+
+void print_statement(p_s **p_p_s)
+{
+	printf("\t\t%s\t",(char *)*p_p_s);
+	printf("%04d/%02d%/%02d\t", *((int *)*p_p_s + 2),*((int *)*p_p_s + 3), *((int *)*p_p_s + 4));
+	printf("%s\t",(char *)*p_p_s + 20);
+	printf("0%d\t\n",*((int *)*p_p_s + 7));
 }
 
 int main()
 {
-	int mat[27] = {0,};
-	int i;
+	int i, num;
+	printf("입력할 명세서의 개수를 입력하시오 : "); 
+	scanf("%d",&num);
+	p_s **p_p_s = (p_s **)malloc(sizeof(p_s *) * num);
+	int *birth = (int *)malloc(sizeof(int *) * num);
+	p_s **p_p_s_first = p_p_s;
 	
-	printf("enter 3 X 3 matrix element : ");
-	for(i = 0; i < 9; i++)
+	for(i = 0; i < num; i++)
 	{
-		scanf("%d",mat+i);
-		*(mat + 9 + 3 * (i % 3) + i / 3) = *(mat + i);
+		printf("개별명세 입력%d : ",i + 1);
+		*p_p_s = (p_s *)malloc(sizeof(p_s));
+		
+		scanf(" %s",(char *)*p_p_s);
+		scanf("%d/%d/%d",(int *)*p_p_s + 2, (int *)*p_p_s + 3, (int *)*p_p_s + 4);
+		*(birth + i) = *((int *)*p_p_s + 2) * 10000 + *((int *)*p_p_s + 3) * 100 + *((int *)*p_p_s + 4);
+		scanf(" %s",(char *)*p_p_s + 20);
+		scanf("%d",(int *)*p_p_s + 7);
+		
+		p_p_s++;
 	}
 	
-	printf("\nsum of matrix\n");
-	indexAddMat(mat);
-	for(i = 0; i < 9; i++)
-	{
-		printf("%4d",*(mat+18+i));
-		if(i % 3 == 2) printf("\n");
-	}
-	printf("\n");
+	printf("\n입력 순 출력\t이름\t생년월일\t성별\t전화번호\n");
+	p_p_s = p_p_s_first;
+	for(i = 0; i < num; i++) print_statement(p_p_s++);
 	
-	for(i = 18; i < 27; i++) *(mat+i) = 0;
+	printf("\n이름 순 출력\t이름\t생년월일\t성별\t전화번호\n");
+	p_p_s = p_p_s_first;
+	name_priority_sort(p_p_s, birth, num);
+	for(i = 0; i < num; i++) print_statement(p_p_s++);
 	
-	printf("product of matrix\n");
-	ptrProdMat(mat);
-	for(i = 0; i < 9; i++)
-	{
-		printf("%4d",*(mat+18+i));
-		if(i % 3 == 2) printf("\n");
-	}
+	printf("\n생일 순 출력\t이름\t생년월일\t성별\t전화번호\n");
+	p_p_s = p_p_s_first;
+	birth_priority_sort(p_p_s, birth, num);
+	for(i = 0; i < num; i++) print_statement(p_p_s++);
 	
 	return 0;
 }
